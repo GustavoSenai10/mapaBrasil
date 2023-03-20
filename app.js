f34a5f335be429982602eca65a76c893d1b41144
Gustavo Souza <GustavoSenai500@gmail.com>
2023-03-20 16:18:13 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const mapa = document.querySelector('svg')
+const getEstados = (event) =>{
+    const estado = (event.target.id.replace('BR-', ''))
+
+    console.log(estado)
+}   
+mapa.addEventListener('click', getEstados)
